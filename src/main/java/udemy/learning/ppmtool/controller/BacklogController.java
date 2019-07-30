2909383d1bc2dc0467b96982ed71c37cdaba3a54
Nikita <JacksonGibsonESP@gmail.com>
2019-07-30 15:15:40 +0300

    Get backlog endpoint and migration to MySql.

--- src/main/java/udemy/learning/ppmtool/controller/BacklogController.java
+++ src/main/java/udemy/learning/ppmtool/controller/BacklogController.java
@@ -33,4 +33,9 @@ public class BacklogController {
 
         return new ResponseEntity<>(projectTask1, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{backlogId}")
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlogId) {
+        return projectTaskService.findBacklogById(backlogId);
+    }
 }
